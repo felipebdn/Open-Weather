c756d5bc69cbc68fa871c1ab8c4a56ac9a45ba3f
Felipe Barbosa <71861370+felipebdn@users.noreply.github.com>
2023-01-30 23:42:20 -0300

    234230012023

--- src/pages/Home/components/WeatherInformation/index.tsx
+++ src/pages/Home/components/WeatherInformation/index.tsx
@@ -1,3 +1,15 @@
+import {
+  ArrowUp,
+  ArrowDown,
+  ThermometerHot,
+  Drop,
+  ArrowUpRight,
+  SunHorizon,
+  ThumbsUp,
+  ThumbsDown,
+  Skull,
+  Leaf,
+} from 'phosphor-react'
 import { useContext } from 'react'
 import { localsContext } from '../../../../context/localsContext'
 import {
@@ -12,52 +24,60 @@ import {
 export function WeatherInformation() {
   const { airPollution, currentWeather, infoLocations } =
     useContext(localsContext)
-  console.log(JSON.stringify(currentWeather, null, '\t'))
+  // console.log(JSON.stringify(currentWeather, null, '\t'))
+
+  function GetHourByUnix(n: number) {
+    return new Date(currentWeather.dt * 1000).toLocaleString('pt-BR', {
+      hour: 'numeric',
+      minute: 'numeric',
+    })
+  }
 
   return (
     <WeatherInformationContainer>
       <TemperatureStatus>
-        <img src="" alt="" />
-        <h1>23,69°</h1>
+        <img
+          src={`https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@4x.png`}
+          alt={currentWeather.weather[0].description}
+        />
+        <h1>{`${currentWeather.main.temp.toFixed(2)}º`}</h1>
         <aside>
           <p>
-            max: <span>23,69°</span>
+            max: <span>{`${currentWeather.main.temp_max.toFixed(0)}º`}</span>
           </p>
           <p>
-            min: <span>23,69°</span>
+            min: <span>{`${currentWeather.main.temp_min.toFixed(0)}º`}</span>
           </p>
         </aside>
       </TemperatureStatus>
       <CurrentData>
-        <h3>Dados atuais - 20:07</h3>
+        <h3>Dados atuais - {GetHourByUnix(currentWeather.dt)}</h3>
         <main>
+          <div>
+            <h2>
+              {`Condições atual: ${currentWeather.weather[0].description}`}
+            </h2>
+          </div>
           <div>
             <p>Temperatura</p>
             <aside>
-              <svg />
-              <span>23º</span>
-              <svg />
-              <span>23º</span>
+              <ArrowUp size={20} color="#FF0000" weight="bold" />
+              <span>{`${currentWeather.main.temp_max}º`}</span>
+              <ArrowDown size={20} color="#0047FF" weight="bold" />
+              <span>{`${currentWeather.main.temp_min}º`}</span>
             </aside>
           </div>
           <div>
             <p>Sensação térmica</p>
             <aside>
-              <svg />
-              <span>23º</span>
-            </aside>
-          </div>
-          <div>
-            <p>Chuva</p>
-            <aside>
-              <svg />
-              <span>8mm</span>
+              <ThermometerHot size={20} color="#DC4F00" weight="thin" />
+              <span>{`${currentWeather.main.feels_like}º`}</span>
             </aside>
           </div>
           <div>
             <p>Vento</p>
             <aside>
-              <svg />
+              <ArrowUpRight size={20} color="#676767" weight="bold" />
               <span>SW - 5km/h</span>
             </aside>
           </div>
@@ -65,7 +85,7 @@ export function WeatherInformation() {
             <p>Sol</p>
             <aside>
               <span>06:35</span>
-              <svg />
+              <SunHorizon size={20} color="#FFD600" />
               <span>06:35</span>
             </aside>
           </div>
@@ -77,32 +97,78 @@ export function WeatherInformation() {
           <div>
             <p>Índice de qualidade do ar</p>
             <aside>
-              <svg />
-              <span>Bom</span>
+              {airPollution.list.map((value) => {
+                switch (value.main.aqi) {
+                  case 1: {
+                    return (
+                      <>
+                        <ThumbsUp size={20} color="#00e109" weight="bold" />
+                        <span>Bom</span>
+                      </>
+                    )
+                  }
+                  case 2: {
+                    return (
+                      <>
+                        <Leaf size={20} color="#7bdc04" weight="bold" />
+                        <span>Regular</span>
+                      </>
+                    )
+                  }
+                  case 3: {
+                    return (
+                      <>
+                        <ThumbsUp size={20} color="#00e109" weight="bold" />
+                        <span>Moderado</span>
+                      </>
+                    )
+                  }
+                  case 4: {
+                    return (
+                      <>
+                        <ThumbsDown size={20} color="#dc6d04" weight="bold" />
+                        <span>Ruim</span>
+                      </>
+                    )
+                  }
+                  case 5: {
+                    return (
+                      <>
+                        <Skull size={32} color="#e02200" weight="bold" />
+                        <span>Muito Ruim</span>
+                      </>
+                    )
+                  }
+                }
+                return null
+              })}
+
+              {/* <ThumbsUp size={20} color="#00e109" weight="bold" />
+              <span>{QualityIndex[airPollution.list[0].main.aqi - 1]}</span> */}
             </aside>
           </div>
           <div>
             <p>Nitrogênio</p>
             <aside>
-              <span>Bom</span>
+              <span>{`${airPollution.list[0].components.no} μg/m³`}</span>
             </aside>
           </div>
           <div>
             <p>Ozônio</p>
             <aside>
-              <span>Bom</span>
+              <span>{`${airPollution.list[0].components.o3} μg/m³`}</span>
             </aside>
           </div>
           <div>
             <p>Dióxido de enxofre</p>
             <aside>
-              <span>Bom</span>
+              <span>{`${airPollution.list[0].components.so2} μg/m³`}</span>
             </aside>
           </div>
           <div>
             <p>Partículas finas</p>
             <aside>
-              <span>Bom</span>
+              <span>{`${airPollution.list[0].components.pm2_5} μg/m³`}</span>
             </aside>
           </div>
         </main>
--- src/pages/Home/components/WeatherInformation/styles.ts
+++ src/pages/Home/components/WeatherInformation/styles.ts
@@ -4,15 +4,123 @@ import { device } from '../../../../Global/GlobalStyle'
 const baseDivDash = styled.div`
   background: ${({ theme }) => theme['base-post']};
   padding: 1rem;
+  border-radius: 10px;
 `
 export const TemperatureStatus = styled(baseDivDash)`
   grid-area: temp;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    width: 100px;
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  aside {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+
+    p {
+      text-transform: uppercase;
+    }
+
+    p:nth-child(1) {
+      color: ${({ theme }) => theme.red};
+    }
+    p:nth-child(2) {
+      color: ${({ theme }) => theme.blue};
+    }
+  }
 `
 export const CurrentData = styled(baseDivDash)`
   grid-area: headerC;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 1.5rem;
+
+  main {
+    flex: 1;
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    div:last-child {
+      border: none;
+    }
+
+    div {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      border-bottom: 2px solid #303030;
+
+      padding: 0.5rem;
+
+      align-items: center;
+
+      p {
+        font-size: 1.5rem;
+      }
+
+      aside {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+      }
+    }
+  }
 `
 export const AirPollution = styled(baseDivDash)`
   grid-area: headerA;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 1.5rem;
+
+  main {
+    flex: 1;
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    div:last-child {
+      border: none;
+    }
+
+    div {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      border-bottom: 2px solid #303030;
+
+      padding: 0.5rem;
+
+      align-items: center;
+
+      p {
+        font-size: 1.3rem;
+      }
+
+      aside {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+      }
+    }
+  }
 `
 export const WeatherLayers = styled(baseDivDash)`
   grid-area: aside;
